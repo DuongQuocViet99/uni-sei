00605c326a40326c74ea4cf6d61b819cf7f89108
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-01-19 12:14:35 +0700

    use ChakraProvider and Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider} from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+
+import HomeIndex from "pages/home/Index";
+import GroupIndex from "pages/group/Index";
+import "App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ChakraProvider>
+        <Routes>
+          <Route path='/' element={ <HomeIndex /> } />
+          <Route path='/:projectid' element={ <GroupIndex /> } />
+        </Routes>
+      </ChakraProvider>
+    </>
   );
 }
 
