1ffd3abe434c13a14aa461c0d72f3e07d510632e
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-02 15:20:56 +0700

    create and render featureList

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,25 +1,37 @@
-import { Grid, GridItem, VStack } from "@chakra-ui/react";
+import { Box, Grid, GridItem, VStack } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { FiHelpCircle, FiPlus, FiSearch, FiStar } from "react-icons/fi";
-import IconButton from "components/IconButton";
-import Drawer from "components/Drawer";
+import IconButtonSC from "components/IconButton";
+import DrawerSC from "components/Drawer";
+import TooltipSC from "components/Tooltip";
+import Map from "render/Map";
+
+const featureList = [
+  { icon: FiPlus, label: 'New'}, 
+  { icon: FiSearch, label: 'Search'}, 
+  { icon: FiStar, label: 'Premium'},
+  { icon: FiHelpCircle, label: 'Q&A'}
+];
 
 export default function Layout() {
   return (
     <>
-      <Grid templateColumns='repeat(12, 1fr)' minH='100vh'>
-        <GridItem bg='#6d6875'>
-          <VStack pt={ 9 } spacing={ 20 } position='sticky' top={ 0 }>
-            <Drawer />
-            <VStack spacing={ 8 } justify='end'>
-              <IconButton color='#ffb4a2' bg='#6d6875' icon={ <FiPlus fontSize='20px' /> } />
-              <IconButton color='#ffb4a2' bg='#6d6875' icon={ <FiSearch fontSize='18px' /> } />
-              <IconButton color='#ffb4a2' bg='#6d6875' icon={ <FiStar fontSize='18px' /> } />
-              <IconButton color='#ffb4a2' bg='#6d6875' icon={ <FiHelpCircle fontSize='18px' /> } />
+      <Grid templateColumns='repeat(16, 1fr)' minH='100vh'>
+        <GridItem borderRight='1px solid rgba(109, 104, 117, 0.2)'>
+          <Box position='sticky' top={ 0 }>
+            <DrawerSC />
+            <VStack mt={ 20 } spacing={ 6 }>
+              <Map data={ featureList } render={( i, k ) => 
+                <TooltipSC key={ k } label={ i.label } placement='right'>
+                  <Box>
+                    <IconButtonSC opacity='0.6' fontSize='20px' icon={ <i.icon/> } />
+                  </Box>
+                </TooltipSC>
+              }/>
             </VStack>
-          </VStack>
+          </Box>
         </GridItem>
-        <GridItem colSpan={ 11 }>
+        <GridItem colSpan={ 15 }>
           <Outlet />
         </GridItem>
       </Grid>
