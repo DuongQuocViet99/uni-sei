728857af26398ce27bb58439a9f8b8e73fbcf193
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-14 14:58:59 +0700

    custom theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
-
 import "App.css";
 import Layout from "pages/Layout";
 import Group from "pages/group/View";
 import Index from "redux/index/Bindings/State.default";
 
 export default function App() {
+  const theme = extendTheme({
+    colors: {
+      sei: {
+        gray: '#6d6875',
+        brown: '#b5838d',
+        red: '#e5989b',
+        orange: '#ffb4a2',
+        yellow: '#ffcdb2',
+      }
+    }
+  });
+
   return (
     <>
-      <ChakraProvider>
+      <ChakraProvider theme={ theme }>
         <Routes>
           <Route path='/' element={ <Layout /> }>
             <Route index element={ <Index /> } />
