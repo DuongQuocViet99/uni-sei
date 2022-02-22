8dde00ea96f934e47e6bdac8c5184bbcd5e892f4
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-22 12:30:52 +0700

    UI: change icon

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,8 +1,14 @@
 import {
-  Drawer, DrawerBody, DrawerHeader, DrawerFooter, Box, DrawerOverlay, DrawerContent, useDisclosure
+  DrawerHeader, 
+  DrawerFooter, 
+  DrawerOverlay, 
+  DrawerContent, 
+  useDisclosure,
+  DrawerBody, 
+  Drawer, 
+  Box
 } from '@chakra-ui/react';
-import { FiMenu, FiX } from 'react-icons/fi';
-
+import { FiBarChart2, FiX } from 'react-icons/fi';
 import IconButtonSC from './IconButton';
 
 export default function DrawerSC() {
@@ -10,13 +16,13 @@ export default function DrawerSC() {
   
   return (
     <>
-      <IconButtonSC
-        m={ 5 }
-        size='lg'  
-        onClick={ onOpen } 
-        color='#6d6875' 
-        fontSize='20px'
-        icon={ <FiMenu/> } 
+      <IconButtonSC 
+        onClick={ onOpen }
+        bg='sei.gray'
+        color='white'  
+        fontSize='2xl'
+        transform='rotate(90deg)'
+        icon={ <FiBarChart2/> } 
       />
       <Drawer isOpen={ isOpen } onClose={ onClose } placement='left'>
         <DrawerOverlay />
