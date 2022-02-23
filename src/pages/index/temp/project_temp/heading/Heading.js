6fd72a809a9d75a6d249ff8b9c904be480030fdb
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-24 05:09:43 +0700

    fix: Editable

--- src/pages/index/temp/project_temp/heading/Heading.js
+++ src/pages/index/temp/project_temp/heading/Heading.js
@@ -1,45 +1,36 @@
-import { Editable, HStack, useBoolean, useToast } from "@chakra-ui/react";
+import { HStack, useBoolean, useToast } from "@chakra-ui/react";
 import { useState } from "react";
-import axios from "axios";
 import HeadingSC from "components/Heading";
 import Preview from "pages/index/temp/project_temp/heading/edit/Preview";
 import Input from "pages/index/temp/project_temp/heading/edit/Input";
 import Controls from "pages/index/temp/project_temp/heading/edit/Controls";
 import ToastSC from "components/Toast";
+import EditableS from "components/Editable";
 
-export default function Heading({ project }) {
-  const [ state, setState ] = useState( project.name );
-  const [ bool, useBool ] = useBoolean();
+export default function Heading({ project, reduxActRename }) {
+  const [ projectName, setProjectName ] = useState( project.name );
+  const [ bool, setBool ] = useBoolean( false );
   const toast = useToast({ duration: 3000, position: "top" });
-
+  
   return (
     <>
       <HeadingSC size='md' letterSpacing="wide">
-        <Editable 
-          isPreviewFocusable={ false } 
-          submitOnBlur={ false }
-          value={ bool ? state : project.name }
-          onEdit={ useBool.on }
-          onCancel={ useBool.off }
-          onChange={ nextValue => setState( nextValue ) }
+        <EditableS
+          value={ bool ? projectName : project.name }
+          onChange={ nextValue => {
+            setBool.on()
+            setProjectName( nextValue )
+          }}
           onSubmit={ name => {
-            axios({ 
-              method: 'PUT', 
-              url: `http://localhost:3001/project/${ project.projectid }/rename`, 
-              data: { name } 
-            })
-            .then( res => res.status === 200 && 
-              toast({
-                render: () => (
-                  <ToastSC 
-                    status='success'
-                    heading='Successfully renamed' 
-                    text="We've renamed your project for you." 
-                  />
-                )
-              })   
-            )
-            useBool.off() 
+            setBool.off()
+            reduxActRename( project, name )
+            toast({ render: () => 
+              <ToastSC 
+                status='success'
+                heading='Successfully renamed' 
+                text="We've renamed your project for you." 
+              />
+            })   
           }}
         >
           <HStack spacing={ 0 }>
@@ -47,7 +38,7 @@ export default function Heading({ project }) {
             <Input />
             <Controls />
           </HStack>
-        </Editable>
+        </EditableS>
       </HeadingSC>
     </>
   );
