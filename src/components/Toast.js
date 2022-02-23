44758a388660febbecd788286100d39b8a3b60c6
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-24 04:13:38 +0700

    delete: icon of status

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,25 +1,32 @@
-import { Box, HStack } from "@chakra-ui/react";
-import { FiCheckCircle, FiInfo, FiXCircle } from "react-icons/fi";
+import { Box, Divider, HStack } from "@chakra-ui/react";
 import HeadingSC from "components/Heading";
 import TextSC from "components/Text";
 import FilterToMap from "render/FilterToMap";
 
 const statusList = [
-  { type: 'success', color: '#89b0ae', icon: FiCheckCircle },
-  { type: 'error', color: '#e5989b', icon: FiXCircle },
-  { type: 'warning', color: '#ffcdb2', icon: FiInfo }
+  { type: 'success', color: '#89b0ae' },
+  { type: 'error', color: 'sei.red' },
+  { type: 'warning', color: 'sei.yellow' }
 ];
 
 export default function ToastSC({ heading, text, status }) {
   return (
     <>
       <FilterToMap data={ statusList } condition={ s => s.type === status } render={( i, k ) => 
-        <Box key={ k } bg={ i.color } p={ 2 } rounded='sm'>
+        <Box key={ k } bg='sei.gray' p={ 2 } rounded='sm'>
           <HStack>
-            <i.icon color='white' fontSize='18px' />
-            <HeadingSC color='white' fontSize='md'>{ heading }</HeadingSC>
+            <Divider 
+              border='5px' 
+              borderColor={ i.color } 
+              orientation='vertical' 
+              h='40px' 
+              rounded='full'
+            />
+            <Box>
+              <HeadingSC color='white' fontSize='md'>{ heading }</HeadingSC>
+              <TextSC fontSize='sm' color='white'>{ text }</TextSC>
+            </Box>
           </HStack>
-          <TextSC fontSize='sm' color='white'>{ text }</TextSC>
         </Box>
       }/>
     </>
