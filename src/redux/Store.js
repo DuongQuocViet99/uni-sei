e37e6a0d46558438ac594a7d9a88a319e56de711
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-27 14:12:37 +0700

    add midd

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import reducerForindex from "redux/index/Reducer";
+import renamed from "redux/index/middlewares/Rename";
+import deleted from "redux/index/middlewares/Delete";
 
 const reducer = combineReducers({ reducerForindex });
 
 export default configureStore({
   reducer,
-  middleware: []
+  middleware: [
+    renamed,
+    deleted
+  ]
 });
