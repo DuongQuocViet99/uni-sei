70215ec77d72d19965a524d315b9db185d251162
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-03-10 10:08:06 +0700

    style: update

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,14 +4,13 @@ export default function InputS( props ) {
   return (
     <>
       <Input 
-        _placeholder={{ fontSize: '15px', color: 'sei.gray' }}
-        _hover={{ bg: 'rgba(109, 104, 117, 0.4)' }}
-        bg='rgba(109, 104, 117, 0.2)'
-        focusBorderColor='sei.gray'
-        fontFamily='Quicksand'
-        fontWeight='black'
-        variant='filled'
         rounded='sm'
+        fontWeight='black'
+        fontFamily='Quicksand'
+        focusBorderColor='sei.gray'
+        borderColor='rgba(109, 104, 117, 0.2)'
+        _hover={{ borderColor: 'rgba(109, 104, 117, 0.4)' }}
+        _placeholder={{ fontSize: '15px', color: 'sei.gray' }}
         { ...props }
       />
     </>
